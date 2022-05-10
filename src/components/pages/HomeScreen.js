44b80799358f23cadbe8a121c3213c90b9371dc5
David <zein207@gmail.com>
2022-05-10 12:40:28 -0500

    History section

--- src/components/pages/HomeScreen.js
+++ src/components/pages/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { History } from '../ui/History'
 import { MainCover } from '../ui/MainCover'
 
 export const HomeScreen = () => {
   return (
     <div>
+      
       <MainCover />
+
+      <History />
+
     </div>
   )
 }
---
+++ src/components/ui/History.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const History = () => {
+  return (
+    <section className='history__wrapper'>
+
+        <h5 className='c-primary'>Tennis</h5>
+
+        <div className='history__text-container'>
+
+            <h3>
+                Raquetas. Accesorios.
+                <br />
+                Pelotas.
+            </h3>
+
+            <p>
+                Conoce nuestras raquetas y aprende a jugar
+                <br />
+                Tennis como un profesional.
+            </p>
+            
+        </div>
+
+
+        <a href='/historia' className='btn btn-invert'>Historia</a>
+    </section>
+  )
+}
